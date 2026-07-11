a9fd4141bd8bb8448199189f401a437365f7ba25
vercim <67692844+vercim@users.noreply.github.com>
2026-07-11 18:06:10 +0300

    Update social.ts

--- src/data/social.ts
+++ src/data/social.ts
@@ -28,7 +28,7 @@ export const socialLinks: SocialEntry[] = [
     id: 'youtube',
     label: 'YouTube',
     handle: '',
-    href: 'https://youtube.com/@kiteax2',
+    href: 'https://youtube.com/@teatthe',
     icon: SiYoutube,
   },
   {
